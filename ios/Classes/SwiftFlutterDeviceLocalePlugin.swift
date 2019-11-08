73000fe8e72a5e637971ce70359787a9e24b4e49
Florin Bratan <florin@bratan.me>
2019-11-08 22:30:29 +0100

    Implemented the iOS call

--- ios/Classes/SwiftFlutterDeviceLocalePlugin.swift
+++ ios/Classes/SwiftFlutterDeviceLocalePlugin.swift
@@ -1,14 +1,25 @@
 import Flutter
 import UIKit
 
-public class SwiftFlutterDeviceLocalePlugin: NSObject, FlutterPlugin {
+public class SwiftFlutterDeviceLocalePlugin: NSObject, FlutterPlugin
+{
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "flutter_device_locale", binaryMessenger: registrar.messenger())
     let instance = SwiftFlutterDeviceLocalePlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+  public func handle(_ call: FlutterMethodCall, result: FlutterResult)
+  {
+    if(call.method == "deviceLocales")
+    {
+        let deviceLocales = NSLocale.preferredLanguages;
+        
+        result(deviceLocales);
+    }
+    else
+    {
+        result(FlutterMethodNotImplemented)
+    }
   }
 }
