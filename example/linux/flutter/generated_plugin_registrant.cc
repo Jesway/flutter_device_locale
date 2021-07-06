09409251bdfd6d1f931a11615bd68a13965d9af6
proninyaroslav <proninyaroslav@gmail.com>
2021-07-06 12:36:20 +0300

    Add Linux support

---
+++ example/linux/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,13 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <flutter_device_locale/flutter_device_locale_plugin.h>
+
+void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) flutter_device_locale_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "FlutterDeviceLocalePlugin");
+  flutter_device_locale_plugin_register_with_registrar(flutter_device_locale_registrar);
+}
